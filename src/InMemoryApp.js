9cb8a2ff49c264be18048315fd3234ab029d1b46
danica-du <ddu@hmc.edu>
2021-10-11 23:08:22 -0700

    onItemChanged/Added/Deleted handlers in InMemoryApp

--- src/InMemoryApp.js
+++ src/InMemoryApp.js
@@ -1,7 +1,32 @@
 import App from './App';
+import { useState } from 'react';
+import { generateUniqueID } from "web-vitals/dist/modules/lib/generateUniqueID";
 
 function InMemoryApp(props) {
-    return <App data={props.initialData}/>
+    const [data, setData] = useState(props.initialData);
+
+    function handleItemChanged(itemID, field, newValue) {
+        setData(data.map((item) => {
+            if (item.id !== itemID) return item[field] = newValue;
+        }));
+    }
+
+    function handleItemAdded(text, isCompleted = false) {
+        setData(data.push({
+            id: generateUniqueID(),
+            text: text,
+            isCompleted: isCompleted,
+        }))
+    }
+
+    function handleItemDeleted(itemID) {
+        setData(data.filter((item) => {
+            return item.id !== itemID
+        }));
+    }
+
+    return (<App data={props.initialData} onItemChanged={handleItemChanged} onItemAdded={handleItemAdded}
+                 onItemDeleted={handleItemDeleted}/>);
 }
 
 export default InMemoryApp;
