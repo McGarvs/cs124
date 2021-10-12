13c598cabfb4126bd0f891a838ac1e6608196523
danica-du <ddu@hmc.edu>
2021-10-11 23:14:33 -0700

    Fixed handleItemChanged

--- src/InMemoryApp.js
+++ src/InMemoryApp.js
@@ -1,14 +1,14 @@
 import App from './App';
-import { useState } from 'react';
-import { generateUniqueID } from "web-vitals/dist/modules/lib/generateUniqueID";
+import {useState} from 'react';
+import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 
 function InMemoryApp(props) {
     const [data, setData] = useState(props.initialData);
 
     function handleItemChanged(itemID, field, newValue) {
-        setData(data.map((item) => {
-            if (item.id !== itemID) return item[field] = newValue;
-        }));
+        setData(data.map((item) =>
+            (item.id !== itemID) ? {...item, [field]:newValue} : {...item}
+        ));
     }
 
     function handleItemAdded(text, isCompleted = false) {
