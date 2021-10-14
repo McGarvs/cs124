a49b51b1f8873e26aecedae886b27ee4c1fa2481
danica-du <ddu@hmc.edu>
2021-10-13 20:23:20 -0700

    Header styling

--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,16 @@ function Header(props) {
     const [text, setText] = useState("");
     return (
         <div id="header">
+            {props.showCompleted ?
+                <div id="master-options">
+                    <div id="show-completed-btn" onClick={props.onShowBtnClick}>Hide Completed</div>
+                    <div id="dlt-all-btn" onClick={props.onDelCompletedModalDisplay}>Delete Completed</div>
+                </div>
+            :
+                <div id="master-options">
+                    <div id="show-completed-btn" onClick={props.onShowBtnClick}>Show Completed</div>
+                </div>
+            }
             <div id="add-new-item">
                 <input type="text" id="input-field" placeholder="Enter a task here!" value={text}
                        onChange={(e) => setText(e.target.value)}/>
@@ -14,20 +24,8 @@ function Header(props) {
                         setText("");
                     }
                 }
-                }>Add Item</div>
+                }>+</div>
             </div>
-                {props.showCompleted ?
-                    <div id="master-options">
-                        <div id="show-completed-btn" onClick={props.onShowBtnClick}>Hide Completed</div>
-                        <div id="dlt-all-btn" onClick={props.onDelCompletedModalDisplay}>Delete Completed</div>
-                    </div>
-                :
-                    <div id="master-options">
-                        <div id="show-completed-btn" onClick={props.onShowBtnClick}>Show Completed</div>
-                    </div>
-                }
-
-
         </div>
     );
 }
