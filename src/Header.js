12143569300894272f5fc177d309bf0377ec0706
danica-du <ddu@hmc.edu>
2021-10-13 20:38:07 -0700

    More header styling, added title

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,22 @@ function Header(props) {
     const [text, setText] = useState("");
     return (
         <div id="header">
-            {props.showCompleted ?
-                <div id="master-options">
-                    <div id="show-completed-btn" onClick={props.onShowBtnClick}>Hide Completed</div>
-                    <div id="dlt-all-btn" onClick={props.onDelCompletedModalDisplay}>Delete Completed</div>
+            <div id="header-top">
+                <div id="header-title">
+                    Todo List
                 </div>
-            :
-                <div id="master-options">
-                    <div id="show-completed-btn" onClick={props.onShowBtnClick}>Show Completed</div>
-                </div>
-            }
+                {props.showCompleted ?
+                    <div id="master-options">
+                        <div id="show-completed-btn" onClick={props.onShowBtnClick}>Hide Completed</div>
+                        <div id="dlt-all-btn" onClick={props.onDelCompletedModalDisplay}>Delete Completed</div>
+                    </div>
+                    :
+                    <div id="master-options">
+                        <div id="show-completed-btn" onClick={props.onShowBtnClick}>Show Completed</div>
+                    </div>
+                }
+            </div>
+
             <div id="add-new-item">
                 <input type="text" id="input-field" placeholder="Enter a task here!" value={text} maxLength="80"
                        onChange={(e) => setText(e.target.value)}/>
