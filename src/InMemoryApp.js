36da0f64fac6bf1fb4386dccc959530550e854c8
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-10-20 13:34:24 -0700

    Firestore dependencies

--- src/InMemoryApp.js
+++ src/InMemoryApp.js
@@ -2,6 +2,19 @@ import App from './App';
 import {useState} from 'react';
 import {generateUniqueID} from "web-vitals/dist/modules/lib/generateUniqueID";
 
+// import firebase from "firebase/compat";
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCcQ6XCOvMIA7pHME4bWBgy_7OVy_7XErA",
+//     authDomain: "cs124-fall2021.firebaseapp.com",
+//     projectId: "cs124-fall2021",
+//     storageBucket: "cs124-fall2021.appspot.com",
+//     messagingSenderId: "264318304667",
+//     appId: "1:264318304667:web:4be8d27a02811b1ccd613e"
+// };
+// firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
+
 function InMemoryApp(props) {
     const [data, setData] = useState(props.initialData);
 
