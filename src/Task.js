260c4fe1af8f16916efa9c48bdaa3b24b49bea1d
danica-du <ddu@hmc.edu>
2021-10-25 23:54:24 -0700

    Priority is no longer hard-coded and can be changed

--- src/Task.js
+++ src/Task.js
@@ -4,10 +4,24 @@ import checkmarkIcon from './static/checkmark.png';
 import editIcon from './static/edit-icon.png';
 import deleteIcon from './static/delete-icon.png';
 
+function repeatString(str, numTimes) {
+    let result = "";
+    while (numTimes > 0) {
+        result += str;
+        numTimes--;
+    }
+    return result;
+}
+
 function Task(props) {
+    const [priority, setPriority] = useState(props.priority)
     const [text, setText] = useState(props.text);
     const [completed, setCompleted] = useState(props.isCompleted);
     const [editing, setEditing] = useState(false);
+
+    function changePriority(newPriority) {
+        setPriority(newPriority);
+    }
 
     return (
         <div className="task-container" style={{flexWrap: editing ? "wrap" : "nowrap"}}>
@@ -23,11 +37,11 @@ function Task(props) {
                     </div>
                 </div>
                 {editing ? <input type="text" className="edit-field" value={text} maxLength="80"
-                                     onChange={(e) => setText(e.target.value)} autoFocus/>
+                                  onChange={(e) => setText(e.target.value)} autoFocus/>
                     :
                     <div className="text-content">
                         <div className="priority">
-                        !!!
+                            {repeatString("!", priority)}
                         </div>
                         {text}
                     </div>
@@ -43,24 +57,28 @@ function Task(props) {
                     <div className="edit-btn" onClick={(e) => {
                         setEditing(true);
                     }}>
-                        <img src={editIcon} alt="edit" />
+                        <img src={editIcon} alt="edit"/>
                     </div>
                     <div className="dlt-btn" onClick={(e) => {
                         props.onDeleteID(props.id);
                         props.onDeleteModalDisplay(true);
                     }}>
-                        <img src={deleteIcon} alt="delete" />
+                        <img src={deleteIcon} alt="delete"/>
                     </div>
                 </div>
             }
 
             {editing &&
-                <div className="priority-btn-container">
-                    <button>None</button>
-                    <button className="selected-priority-btn">!</button>
-                    <button>!!</button>
-                    <button>!!!</button>
-                </div>
+            <div className="priority-btn-container">
+                <button className={priority === 0 && "selected-priority-btn"} onClick={() => changePriority(0)}>None
+                </button>
+                <button className={priority === 1 && "selected-priority-btn"} onClick={() => changePriority(1)}>!
+                </button>
+                <button className={priority === 2 && "selected-priority-btn"} onClick={() => changePriority(2)}>!!
+                </button>
+                <button className={priority === 3 && "selected-priority-btn"} onClick={() => changePriority(3)}>!!!
+                </button>
+            </div>
             }
 
         </div>
