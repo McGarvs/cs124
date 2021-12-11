7c4fbfc69497d4219c23472ead13716867588883
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-12-11 02:10:54 -0800

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -23,6 +23,7 @@ function Header(props) {
                                onChange={(e) => setText(e.target.value)}
                                />
                         <button type="submit" id={(text !== "")?"add-btn-active":"add-btn-disabled"}
+                                className={(text !== "")?"text-btn btn-enabled":"text-btn btn-disabled"}
                                 tabIndex={props.modalDisplayed || (text === "") ? "-1" : ""}
                                 aria-label="Create task">
                             +
